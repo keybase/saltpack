c6eb6c4b7c7a1a06d9c96e37e65038c497dd8e91
Frederick Akalin <akalin@gmail.com>
2017-04-14 13:58:36 -0700

    Add more nonce tests

--- nonce_test.go
+++ nonce_test.go
@@ -7,12 +7,33 @@ import (
 	"testing"
 )
 
-func TestNonceForPayloadKeyBoxV2(t *testing.T) {
-	nonce1 := nonceForPayloadKeyBoxV2(0)
-	nonce2 := nonceForPayloadKeyBoxV2(1)
+func TestNonceForPayloadKeyBoxV1(t *testing.T) {
+	nonce1 := nonceForPayloadKeyBox(Version1(), 0)
+	nonce2 := nonceForPayloadKeyBox(Version1(), 1)
 
-	if nonce2 == nonce1 {
-		t.Errorf("nonce2 == nonce1 == %v unexpectedly", nonce1)
+	// The V1 MAC key doesn't depend on the index; this is fixed
+	// in V2.
+	if nonce2 != nonce1 {
+		t.Errorf("nonce2 == %v != nonce1 == %v unexpectedly", nonce2, nonce1)
+	}
+}
+
+func TestNonceForPayloadKeyBoxV2(t *testing.T) {
+	nonce1a := nonceForPayloadKeyBoxV2(0)
+	nonce1b := nonceForPayloadKeyBox(Version2(), 0)
+	nonce2a := nonceForPayloadKeyBoxV2(1)
+	nonce2b := nonceForPayloadKeyBox(Version2(), 1)
+
+	if nonce1b != nonce1a {
+		t.Errorf("nonce1b == %v != nonce1a == %v unexpectedly", nonce1b, nonce1a)
+	}
+
+	if nonce2b != nonce2a {
+		t.Errorf("nonce2b == %v != nonce2a == %v unexpectedly", nonce2b, nonce2a)
+	}
+
+	if nonce2a == nonce1a {
+		t.Errorf("nonce2a == nonce1a == %v unexpectedly", nonce1a)
 	}
 }
 
