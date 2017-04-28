1906eaad3b897cbfe392fcd54adb57c29b26e5e3
Frederick Akalin <akalin@gmail.com>
2017-04-27 17:07:54 -0700

    Make names consistent

--- decrypt.go
+++ decrypt.go
@@ -49,7 +49,7 @@ func (ds *decryptStream) getNextChunk() ([]byte, error) {
 		return nil, err
 	}
 
-	chunk, err := ds.processEncryptionBlock(ciphertext, authenticators, isFinal, seqno)
+	chunk, err := ds.processBlock(ciphertext, authenticators, isFinal, seqno)
 	if err != nil {
 		return nil, err
 	}
@@ -82,7 +82,7 @@ func (ds *decryptStream) readHeader(rawReader io.Reader) error {
 		return err
 	}
 	header.seqno = seqno
-	err = ds.processEncryptionHeader(&header)
+	err = ds.processHeader(&header)
 	if err != nil {
 		return err
 	}
@@ -179,7 +179,7 @@ func (ds *decryptStream) tryHiddenReceivers(hdr *EncryptionHeader, ephemeralKey 
 	return nil, nil, -1, nil
 }
 
-func (ds *decryptStream) processEncryptionHeader(hdr *EncryptionHeader) error {
+func (ds *decryptStream) processHeader(hdr *EncryptionHeader) error {
 	if err := hdr.validate(ds.versionValidator); err != nil {
 		return err
 	}
@@ -260,7 +260,7 @@ func computeMACKeyReceiver(version Version, index uint64, secret BoxSecretKey, p
 	}
 }
 
-func (ds *decryptStream) processEncryptionBlock(ciphertext []byte, authenticators []payloadAuthenticator, isFinal bool, seqno packetSeqno) ([]byte, error) {
+func (ds *decryptStream) processBlock(ciphertext []byte, authenticators []payloadAuthenticator, isFinal bool, seqno packetSeqno) ([]byte, error) {
 
 	blockNum := encryptionBlockNumber(seqno - 1)
 
