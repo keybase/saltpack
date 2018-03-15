7f745b6d2b9983dc8f1079e0a001e7cd9cad13b2
Frederick Akalin <akalin@keyba.se>
2018-03-14 18:02:50 -0700

    Adapt shuffle

--- common.go
+++ common.go
@@ -45,6 +45,66 @@ func cryptorandRead(b []byte) error {
 	return nil
 }
 
+func cryptorandUint32() (uint32, error) {
+	var buf [4]byte
+	err := cryptorandRead(buf[:])
+	if err != nil {
+		return 0, err
+	}
+
+	return binary.BigEndian.Uint32(buf[:]), nil
+}
+
+// uint32n returns, as an int32, a non-negative pseudo-random number in [0,n).
+// n must be > 0, but int31n does not check this; the caller must ensure it.
+// int31n exists because Int31n is inefficient, but Go 1 compatibility
+// requires that the stream of values produced by math/rand remain unchanged.
+// int31n can thus only be used internally, by newly introduced APIs.
+//
+// For implementation details, see:
+// https://lemire.me/blog/2016/06/27/a-fast-alternative-to-the-modulo-reduction
+// https://lemire.me/blog/2016/06/30/fast-random-shuffling
+func uint32n(n uint32) (uint32, error) {
+	v, err := cryptorandUint32()
+	if err != nil {
+		return 0, err
+	}
+	prod := uint64(v) * uint64(n)
+	low := uint32(prod)
+	if low < uint32(n) {
+		thresh := uint32(-n) % uint32(n)
+		for low < thresh {
+			v, err = cryptorandUint32()
+			if err != nil {
+				return 0, err
+			}
+			prod = uint64(v) * uint64(n)
+			low = uint32(prod)
+		}
+	}
+	return uint32(prod >> 32), err
+}
+
+// shuffle pseudo-randomizes the order of elements.
+// n is the number of elements. Shuffle panics if n < 0.
+// swap swaps the elements with indexes i and j.
+//
+// shuffle is adapted from math/rand.Shuffle from go 1.10.
+func shuffle(n int, swap func(i, j int)) error {
+	if n < 0 || n > ((1<<31)-1) {
+		panic("invalid argument to Shuffle")
+	}
+
+	for i := n - 1; i > 0; i-- {
+		j, err := uint32n(uint32(i + 1))
+		if err != nil {
+			return err
+		}
+		swap(i, int(j))
+	}
+	return nil
+}
+
 type cryptoSource struct {
 	lastErr error
 }
